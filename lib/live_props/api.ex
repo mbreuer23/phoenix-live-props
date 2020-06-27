3f6fe4fb72cea7301024ab2881485a0042cfa9c0
Michael Breuer <breuer.mike@gmail.com>
2020-06-27 14:44:34 -0400

    refactor

--- lib/live_props/api.ex
+++ lib/live_props/api.ex
@@ -3,8 +3,6 @@ defmodule LiveProps.API do
   The LiveProps API.  Exposes two macros, `prop/3` and `state/3` which
   can be used inside Phoenix LiveViews and/or Phoenix LiveComponents
   """
-  alias LiveProps.Utils
-
   defmacro __using__(include: include) do
     arities = %{
       prop: [2, 3],
@@ -12,7 +10,7 @@ defmodule LiveProps.API do
     }
 
     functions = for func <- include, arity <- arities[func], into: [], do: {func, arity}
-    attribute_names = for func <- include, into: [], do: derive_attribute_name(func)
+    attribute_names = for func <- include, into: [], do: prefix(func)
 
     quote do
       import unquote(__MODULE__), only: unquote(functions)
@@ -26,56 +24,152 @@ defmodule LiveProps.API do
 
   defmacro __before_compile__(env) do
     [
-      quoted_prop_api(env)
+      quoted_prop_api(env),
+      quoted_state_api(env)
     ]
   end
 
+  @doc """
+  Define a property and given name and type. Returns `:ok`
+  """
   defmacro prop(name, type, opts \\ []) do
-    build_attribute_ast(:live_prop, name, type, opts, __CALLER__)
-  end
-
-  defmacro state(name, type, opts \\ []) do
-    build_attribute_ast(:live_state, name, type, opts, __CALLER__)
-  end
-
-  defp build_attribute_ast(attribute, name, type, opts, caller) do
     quote do
-      attribute = unquote(attribute)
-      name = unquote(name)
-      type = unquote(type)
-      opts = unquote(opts)
-      caller = unquote(Macro.escape(caller))
-
-      definition = LiveProps.Factory.build_attribute(attribute, caller, name, type, opts)
-
-      Module.put_attribute(
-        __MODULE__,
-        attribute,
-        definition
-      )
+      LiveProps.API.__prop__(unquote(name), unquote(type), unquote(opts), __MODULE__)
     end
   end
 
-  defp derive_attribute_name(type) when is_atom(type) do
-    ("live_" <> Atom.to_string(type))
-    |> String.to_atom()
+  @doc """
+  Define state of given name and type.  Returns :ok
+  """
+  defmacro state(name, type, opts \\ []) do
+    quote do
+      LiveProps.API.__state__(unquote(name), unquote(type), unquote(opts), __MODULE__)
+    end
+  end
+
+  def __prop__(name, type, opts, module) do
+    define(:prop, name, type, opts, module)
+  end
+
+  def __state__(name, type, opts, module) do
+    define(:state, name, type, opts, module)
+  end
+
+  def __put_default_props__(assigns, props) do
+    props
+    |> Stream.filter(&(&1.has_default == true))
+    |> Enum.reduce(assigns, fn prop, assigns ->
+      Map.put_new_lazy(assigns, prop.name, fn -> prop.default end)
+    end)
+  end
+
+  def __put_computed_props__(assigns, props) do
+    props
+    |> Stream.filter(&(&1.is_computed == true))
+    |> Enum.reduce(assigns, fn prop, assigns ->
+      Map.put(assigns, prop.name, prop.compute.(assigns))
+    end)
+  end
+
+  def __put_default_states__(assigns, states) do
+    states
+    |> Stream.filter(&(&1.has_default == true))
+    |> Enum.reduce(assigns, fn state, assigns ->
+      Map.put(assigns, state.name, state.default)
+    end)
+  end
+
+  def __put_computed_states__(assigns, states, filter \\ nil) do
+    states =
+      case is_function(filter) do
+        true ->
+          states
+          |> Stream.filter(&(&1.is_computed == true))
+          |> Stream.filter(filter)
+
+        false ->
+          states
+          |> Stream.filter(&(&1.is_computed == true))
+      end
+
+    Enum.reduce(states, assigns, fn state, assigns ->
+      Map.put(assigns, state.name, state.compute.(assigns))
+    end)
+  end
+
+  defp define(attribute, name, type, opts, module) do
+    already_defined_names =
+      Module.get_attribute(module, prefix(attribute))
+      |> Enum.map(& &1.name)
+
+    if name in already_defined_names do
+      raise ArgumentError, "#{attribute} with name \"#{name}\" defined more than once."
+    end
+
+    put_attribute!(attribute, name, type, opts, module)
+  end
+
+  defp put_attribute!(attribute, name, type, opts, module) do
+    definition = LiveProps.Factory.build_attribute!(attribute, name, type, opts, module)
+    Module.put_attribute(module, prefix(attribute), definition)
   end
 
   defp quoted_prop_api(env) do
-    props = Utils.get_attributes_in_order_defined(env.module, :live_prop)
+    props = Module.get_attribute(env.module, prefix(:prop), []) |> Enum.reverse()
+    default_props = for p <- props, p[:has_default] == true, do: p
+    computed_props = for p <- props, p[:is_computed] == true, do: p
+    required_props = for p <- props, p[:required] == true, do: p
 
     quote do
-      def __props__ do
-        unquote(Macro.escape(props))
+      def __props__(:all), do: unquote(Macro.escape(props))
+      def __props__(:defaults), do: unquote(Macro.escape(default_props))
+      def __props__(:computed), do: unquote(Macro.escape(computed_props))
+      def __props__(:required), do: unquote(Macro.escape(required_props))
+
+      def __put_default_props__(assigns) do
+        LiveProps.API.__put_default_props__(assigns, __props__(:defaults))
       end
 
-      def __get_prop_by_name__(name) when is_atom(name) do
-        LiveProps.Prop.get_prop_by_name(__props__(), name)
-      end
-
-      def __assign_defaults_props__(socket) do
-        LiveProps.Prop.assign_default_props(socket, __props__())
+      def __put_computed_props__(assigns) do
+        LiveProps.API.__put_computed_props__(assigns, __props__(:computed))
       end
     end
+  end
+
+  defp quoted_state_api(env) do
+    states = Module.get_attribute(env.module, prefix(:state), []) |> Enum.reverse()
+    default_states = for s <- states, s[:has_default] == true, do: s
+    computed_states = for s <- states, s[:is_computed] == true, do: s
+
+    quote do
+      def __states__(:all), do: unquote(Macro.escape(states))
+      def __states__(:defaults), do: unquote(Macro.escape(default_states))
+      def __states__(:computed), do: unquote(Macro.escape(computed_states))
+
+      def __put_default_states__(assigns) do
+        LiveProps.API.__put_default_states__(assigns, __states__(:defaults))
+      end
+
+      def __put_computed_states__(assigns) do
+        LiveProps.API.__put_computed_states__(
+          assigns,
+          __states__(:computed),
+          &(&1[:after_connect] != true)
+        )
+      end
+
+      def __put_async_states__(assigns) do
+        LiveProps.API.__put_computed_states__(
+          assigns,
+          __states__(:computed),
+          &(&1[:after_connect] == true)
+        )
+      end
+    end
+  end
+
+  defp prefix(atom) when is_atom(atom) do
+    ("liveprop_" <> Atom.to_string(atom))
+    |> String.to_atom()
   end
 end

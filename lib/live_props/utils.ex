2cb248122d5ac0b5217b6326e9a1f3f8b8c70a07
Michael Breuer <breuer.mike@gmail.com>
2020-06-27 14:47:29 -0400

    delete old files

--- lib/live_props/utils.ex
+++
@@ -1,9 +0,0 @@
-defmodule LiveProps.Utils do
-  def get_attributes_in_order_defined(module, attribute) do
-    Module.get_attribute(module, attribute, []) |> Enum.reverse()
-  end
-
-  def get_attributes_with_key(attribute_list, key) do
-    for attr <- attribute_list, Map.has_key?(attr, key), do: attr
-  end
-end

f01171389bd64890737774763f45ebda3b860a25
Michael Breuer <breuer.mike@gmail.com>
2020-06-28 15:40:43 -0400

    define delegates

--- lib/live_props.ex
+++ lib/live_props.ex
@@ -2,4 +2,9 @@ defmodule LiveProps do
   @moduledoc """
   Documentation for `LiveProps`.
   """
+  require LiveProps.API
+
+  defdelegate set_state(socket, assigns), to: LiveProps.API
+
+  defdelegate send_state(module, id, assigns), to: LiveProps.API
 end

f4fd12d07597dbe405e1a5ae6d67d5bf7de6a760
Michael Breuer <breuer.mike@gmail.com>
2020-06-28 14:59:32 -0400

    initial live component

---
+++ lib/live_props/live_component.ex
@@ -0,0 +1,105 @@
+defmodule LiveProps.LiveComponent do
+  import Phoenix.LiveView
+  defmacro __using__(_) do
+    quote do
+      use LiveProps.API, include: [:state, :prop]
+
+      @before_compile unquote(__MODULE__)
+    end
+  end
+
+  defmacro __before_compile__(env) do
+    [
+      quoted_update(env),
+      quoted_mount(env)
+    ]
+  end
+
+  defp quoted_update(env) do
+    if Module.defines?(env.module, {:update, 2}) do
+      quote do
+        defoverridable update: 2
+
+        def update(%{lp_command: :set_state} = assigns, socket) do
+          LiveProps.LiveComponent.__update__(assigns, socket, __MODULE__)
+        end
+
+        def update(assigns, socket) do
+          {:ok, socket} = LiveProps.LiveComponent.__update__(assigns, socket, __MODULE__)
+          super(assigns, socket)
+        end
+      end
+    else
+      quote do
+        def update(assigns, socket) do
+          LiveProps.LiveComponent.__update__(assigns, socket, __MODULE__)
+        end
+      end
+    end
+  end
+
+  defp quoted_mount(env) do
+    if Module.defines?(env.module, {:mount, 1}) do
+      quote do
+        defoverridable mount: 1
+
+        def mount(socket) do
+          {:ok, socket} = LiveProps.LiveComponent.__mount__(socket, __MODULE__)
+          super(socket)
+        end
+      end
+    else
+      quote do
+        def mount(socket) do
+          LiveProps.LiveComponent.__mount__(socket, __MODULE__)
+        end
+      end
+    end
+  end
+
+  def __update__(%{lp_command: :set_state} = assigns, socket, module) do
+    assigns =
+      assigns
+      |> Map.drop([:lp_command])
+      |> module.__put_computed_states__()
+      |> module.__put_async_states__()
+
+    {:ok, assign(socket, assigns)}
+  end
+
+  def __update__(assigns, socket, module) do
+    # TODO: check for required props using module.__props__(:required)
+    assigns =
+      assigns
+      |> drop_states(module)
+      |> module.__put_default_props__()
+      |> module.__put_computed_props__()
+
+    {:ok, assign(socket, assigns)}
+  end
+
+  def __mount__(socket, module) do
+    # TODO: As of now async states are not treated any
+    # differently in a live component, where socket should already be connected.
+    # Should be warn the user if they try to user :after_connect option
+    # in a component?
+
+    assigns =
+      socket.assigns
+      |> module.__put_default_states__()
+      |> module.__put_computed_states__()
+      |> module.__put_async_states__()
+
+
+    {:ok, assign(socket, assigns)}
+  end
+
+  # def send_state(module, id, assigns) do
+  #   send self(), {:liveprops, :send_state, [module, id, assigns]}
+  # end
+
+  defp drop_states(assigns, module) do
+    states = for s <- module.__states__(:all), do: s.name
+    Map.drop(assigns, states)
+  end
+end
---
+++ test/live_props/live_component_test.exs
@@ -0,0 +1,86 @@
+defmodule LiveProps.LiveComponentTest do
+  use ExUnit.Case
+  alias Phoenix.LiveView.Socket
+
+  describe "LiveComponent" do
+    defmodule Component do
+      use Phoenix.LiveComponent
+      use LiveProps.LiveComponent
+
+      prop :id, :atom
+      prop :authenticated, :boolean, default: false
+      prop :count, :integer, compute: :get_count
+
+      state :ready, :boolean, default: false
+      state :items, :list, compute: :get_items
+
+      def render(assigns) do
+        ~L"""
+        <%= @ready %>
+        """
+      end
+
+      def update(_assigns, socket) do
+        {:ok, assign(socket, :custom_update, true)}
+      end
+
+      def mount(socket) do
+        {:ok, assign(socket, :custom_mount, true)}
+      end
+
+      def get_count(_), do: 10
+      def get_items(assigns) do
+        case assigns[:ready] do
+          :updated ->
+            [:updated_items]
+          _ ->
+            [:items]
+        end
+      end
+    end
+
+    test "mounts properly" do
+      {:ok, socket} = Component.mount(%Socket{})
+
+      # defaults assigned
+      assert socket.assigns[:ready] == false
+      assert socket.assigns[:items] == [:items]
+
+      # custom mount is respected
+      assert socket.assigns[:custom_mount] == true
+
+      # props not yet available
+      for k <- [:id, :authenticated, :count] do
+        assert Map.has_key?(socket.assigns, k) == false
+      end
+
+    end
+
+    test "updates correctly" do
+      {:ok, socket} = Component.update(%{authenticated: true, ready: :ready}, %Socket{})
+
+      # passes props
+      assert socket.assigns.authenticated == true
+
+      # blocks states from being passed in
+      assert Map.has_key?(socket.assigns, :ready) == false
+
+      # respects custom update
+      assert socket.assigns.custom_update == true
+    end
+
+    test "allows state to be passed in with :lp_command" do
+      {:ok, socket} = Component.mount(%Socket{})
+      assert socket.assigns.ready == false
+      assert socket.assigns.items == [:items]
+
+      {:ok, socket} = Component.update(%{
+        lp_command: :set_state,
+        ready: :updated
+      }, socket)
+
+      assert socket.assigns.ready == :updated
+      assert socket.assigns.items == [:updated_items]
+    end
+  end
+end

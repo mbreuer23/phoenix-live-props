da43bf749c25bad41697e02623df12db985320ff
Michael Breuer <breuer.mike@gmail.com>
2020-06-28 18:35:12 -0400

    add integration tests

---
+++ config/test.exs
@@ -0,0 +1,14 @@
+use Mix.Config
+
+config :phoenix, :json_library, Jason
+
+# config :live_props, Endpoint,
+#   url: [host: "localhost"],
+#   secret_key_base: "Zl9tsLriMkMp4NS44c6hFOYSO6ogsj8I2jE34u9rTdSt40fS1jK2AdPTZG0nI/ND",
+#   live_view: [signing_salt: "ycFcXbhU"]
+
+#   # render_errors: [view: CervantesWeb.ErrorView, accepts: ~w(html json), layout: false],
+#   # pubsub_server: Cervantes.PubSub,
+# config :live_props, Endpoint,
+#   http: [port: 4002],
+#   server: false
---
+++ test/live_props/integration/liveview_test.exs
@@ -0,0 +1,109 @@
+defmodule LiveProps.IntegrationTest.LiveView do
+  use ExUnit.Case
+  # import Plug.Conn
+  # import Phoenix.ConnTest
+  import Phoenix.LiveViewTest
+  @endpoint Endpoint
+
+  defmodule Parent do
+    use Phoenix.LiveView
+    use LiveProps.LiveView
+    alias LiveProps.IntegrationTest.LiveView.Child1
+
+    state :user_id, :integer, default: 1
+    state :show_comments, :boolean, default: false
+
+    def render(assigns) do
+      ~L"""
+      Welcome user <%= @user_id %>
+
+      <button phx-click="toggle" id="toggle">Show Comments</button>
+      <button phx-click="update", id="update">Update comments</button>
+
+      <%= live_component @socket, Child1,
+        id: "child1",
+        user_id: @user_id,
+        show_comments: @show_comments %>
+      """
+    end
+
+    def handle_event("toggle", _, socket) do
+      {:noreply, Phoenix.LiveView.assign(socket, :show_comments, !socket.assigns.show_comments)}
+    end
+
+    def handle_event("update", _, socket) do
+      send_state(Child1, "child1", comments: [:updated_comments])
+      {:noreply, socket}
+    end
+  end
+
+  defmodule Child1 do
+    use Phoenix.LiveComponent
+    use LiveProps.LiveComponent
+
+    prop :user_id, :integer, default: nil
+    prop :show_comments, :boolean, default: true
+
+    state :comments, :list, default: [:comment1, :comment2]
+    state :comment_count, :count, compute: :get_count
+
+    def render(assigns) do
+      ~L"""
+      <div>
+        <%= if @show_comments do %>
+          <div>List comments (count: <%= @comment_count %>)</div>
+          <%= for c <- @comments do %>
+            <%= inspect(c) %>
+          <% end %>
+        <% end %>
+        <button id="set-state" phx-target="<%= @myself %>" phx-click="set-state">Set comments</button>
+      </div>
+      """
+    end
+
+    def handle_event("set-state", _, socket) do
+      {:noreply, set_state(socket, %{comments: [1, 2, 3, 4]})}
+    end
+
+    def get_count(assigns) do
+      length(assigns[:comments])
+    end
+  end
+
+  setup do
+    [conn: Phoenix.ConnTest.build_conn()]
+  end
+
+  test "works", %{conn: conn} do
+    {:ok, view, html} = live_isolated(conn, Parent)
+    assert html =~ "Welcome user 1"
+    refute html =~ "List comments"
+
+    html =
+      view
+      |> element("#toggle")
+      |> render_click()
+
+    assert html =~ "List comments"
+    assert html =~ ":comment1"
+    assert html =~ "count: 2"
+
+    view
+    |> element("#update")
+    |> render_click()
+
+    html = view |> render()
+    assert html =~ ":updated_comments"
+    assert html =~ "count: 1"
+
+
+    html =
+      view
+      |> element("#set-state")
+      |> render_click()
+
+    assert html =~ "count: 4"
+
+
+  end
+end

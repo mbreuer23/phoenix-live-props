a75610b54d9243558828eb13b7e00ce04d63818e
Michael Breuer <breuer.mike@gmail.com>
2020-07-01 13:22:42 -0400

    define conn_case

---
+++ test/support/conn_case.ex
@@ -0,0 +1,31 @@
+defmodule LiveProps.ConnCase do
+  @moduledoc """
+  This module defines the test case to be used by
+  tests that require setting up a connection.
+
+  Such tests rely on `Phoenix.ConnTest` and also
+  import other functionality to make it easier
+  to build common data structures and query the data layer.
+  """
+
+  use ExUnit.CaseTemplate
+
+  using do
+    quote do
+      # Import conveniences for testing with connections
+      # import Plug.Conn
+      import Phoenix.ConnTest
+      import LiveProps.ConnCase
+      import Phoenix.LiveViewTest
+
+      # alias CervantesWeb.Router.Helpers, as: Routes
+
+      # The default endpoint for testing
+      @endpoint Endpoint
+    end
+  end
+
+  setup do
+    {:ok, conn: Phoenix.ConnTest.build_conn()}
+  end
+end
